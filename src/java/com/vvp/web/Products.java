de01da46a5bfa2b4a7eedbc77719bf6f8b6e35db
Srujan Oza <47661651+srujanoza@users.noreply.github.com>
2020-02-18 16:30:29 +0530

    Add files via upload

---
+++ src/java/com/vvp/web/Products.java
@@ -0,0 +1,67 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.vvp.web;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class Products
+{
+
+   public int productId;
+   public String productName;
+   public int productQuantity;
+   public int productPrice;
+   public Products(int productId, String productName,int productQuantity,int productPrice)
+   {
+         this.productId =  productId ;
+         this.productName = productName  ;
+         this.productQuantity = productQuantity ;
+         this.productPrice =  productPrice;
+   }
+    public void setProductId(int productId) {
+        this.productId = productId;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public void setProductQuantity(int productQuantity) {
+        this.productQuantity = productQuantity;
+    }
+
+    public void setProductPrice(int productPrice) {
+        this.productPrice = productPrice;
+    }
+
+    public int getProductId() {
+        return productId;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public int getProductQuantity() {
+        return productQuantity;
+    }
+
+    public int getProductPrice() {
+        return productPrice;
+    }
+  
+   
+
+}
